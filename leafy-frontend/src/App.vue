e2517f5ed293974daa9bc04bd4c4aa9f4634eacd
dkdi4224@naver.com <dkdi4224@naver.com>
2024-04-29 12:46:02 +0900

    뷰 테스트

--- leafy-frontend/src/App.vue
+++ leafy-frontend/src/App.vue
@@ -1,8 +1,8 @@
-<template>
+<template> 
   <div id="app">
     <div class="welcome-message" v-if="isLoggedIn">
       <p class="welcome-text">안녕하세요, <span class="user-name">{{ user.name }}</span>님!</p>
-      <p class="description">오늘도 즐거운 식물 관리하세요.</p>
+      <p class="description">leafy 2차,,, 어휴</p>
     </div>
     <div class="brand" v-if="isLoggedIn">
       LEAFY
